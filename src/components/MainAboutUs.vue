f7aa60d6ac89f0196f90ac5b5eaa761faaabc7c5
Ignacio-Us <i.uribe01@ufromail.cl>
2024-05-08 17:14:12 -0400

    Creacion de HomePage y otras modificaciones
    Se crearon los complementos MainNavbar y MainAboutUs.
    
    Se creo la pagina HomePage como parte de la estructura inicial del proyecto.
    
    Se establecio un directorio router con el archivo 'router.js', para gestionar las rutas y la navegación dentro de la aplicación.
    
    Se agrego la biblioteca de bootstrap al proyecto, tanto en el archivo 'package.json' como en 'package-lock.json'.
    
    Se añadieron las imagenes que se utilizarán en la aplicación a la carpeta de assets.
    
    Por ultimo se importo la biblioteca de bootstrap  y el 'router.js', en el archivo 'main.js'.

---
+++ src/components/MainAboutUs.vue
@@ -0,0 +1,35 @@
+<template>
+    <footer class="container-fluid align-items-center footer-div bg-1">
+        <div class="container-fluid text-white">
+            <div class="row">
+                <div class="col-4 text-start p-2">
+                    <h5>Soporte técnico</h5>
+                    <br />
+                    <p>
+                        <span class="fw-semibold">Número de contacto:</span> 780 2554 5940
+                        <br />
+                        <span class="fw-semibold">Correo electrónico:</span>
+                        support@vegahosting.com
+                    </p>
+                </div>
+
+                <div class="col-6 text-end p-2">
+                    <br /><br />
+                    <RouterLink class="text-white fw-semibold">Preguntas frecuentes</RouterLink>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<style scoped>
+.bg-1 {
+    background-color: #043d6c;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+}
+</style>
+
+<script setup>
+</script>
