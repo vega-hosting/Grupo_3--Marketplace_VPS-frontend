b3af43b0722e3ef11a86c86d38dc085187361abe
Ignacio-Us <i.uribe01@ufromail.cl>
2024-05-14 12:58:02 -0400

    Creacion de la parte visual de ConfigVpsPage
    Se creo la parte visual de la pagina 'ConfigVpsPage'. Tambien se agrego una ruta hacia la pagina dentro del archivo 'router.js'.

--- src/router/router.js
+++ src/router/router.js
@@ -4,16 +4,17 @@ import FaqPage from "@/pages/FaqPage.vue";
 import LoginPage from "@/pages/LoginPage.vue";
 import LoginPageAdmin from "@/pages/LoginPageAdmin.vue";
 import RegisterUser from "@/pages/RegisterUser.vue";
-import PlansPage from "@/pages/PlansPage.vue"
-
+import PlansPage from "@/pages/PlansPage.vue";
+import ConfigVpsPage from "@/pages/ConfigVpsPage.vue";
 
 const routes = [
   { path: "/", component: HomePage },
   { path: "/faq", component: FaqPage },
-  { path: "/login", component: LoginPage},
+  { path: "/login", component: LoginPage },
   { path: "/loginAdmin", component: LoginPageAdmin },
   { path: "/register", component: RegisterUser },
-  { path: "/plans", component: PlansPage}
+  { path: "/plans", component: PlansPage },
+  { path: "/config-vps", component: ConfigVpsPage },
 ];
 
 export const router = createRouter({
